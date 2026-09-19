5b61f165abb0e48bbf2d30b504967f355e42c69f
FranklinChang08 <franklinchang0129@gmail.com>
2026-09-19 18:47:31 +0700

    Add & Update - Merubah tambilan counter dan menambahkan 3 tombol yang berfungsi menambahkan, mengurangi dan mereset counter ke 0

--- lib/main.dart
+++ lib/main.dart
@@ -52,10 +52,24 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _decrimentCounter() {
+    setState(() {
+      {
+        _counter--;
+      }
+    });
+  }
+
+  void _resetCounter() {
+    setState(() {
+      _counter = 0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
+      appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -73,10 +87,94 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: .center,
           children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Container(
+              width: 150,
+              height: 150,
+              decoration: BoxDecoration(
+                color: Colors.blue.withOpacity(0.3),
+                border: Border.all(color: Colors.blue, width: 2.0),
+                shape: BoxShape.circle,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black12,
+                    blurRadius: 5,
+                    offset: Offset(5, 5),
+                  ),
+                ],
+              ),
+
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text('Nilai Counter'),
+                    Text(
+                      '$_counter',
+                      style: TextStyle(
+                        fontSize: 50,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 50),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              spacing: 48,
+              children: [
+                OutlinedButton.icon(
+                  onPressed: _incrementCounter,
+                  style: OutlinedButton.styleFrom(
+                    backgroundColor: Colors.blue.withOpacity(0.8),
+                    foregroundColor: Colors.white,
+                    side: BorderSide(color: Colors.blue, width: 2),
+                    padding: EdgeInsets.symmetric(horizontal: 30),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+
+                  icon: Icon(Icons.add),
+                  label: const Text(
+                    'Tambah',
+                    style: TextStyle(fontWeight: FontWeight.w900),
+                  ),
+                ),
+                OutlinedButton.icon(
+                  onPressed: _decrimentCounter,
+                  style: OutlinedButton.styleFrom(
+                    backgroundColor: Colors.red.withOpacity(0.8),
+                    foregroundColor: Colors.white,
+                    padding: EdgeInsets.symmetric(horizontal: 30),
+                    side: BorderSide(color: Colors.red, width: 2),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  icon: Icon(Icons.remove),
+                  label: const Text('Kurang'),
+                ),
+              ],
+            ),
+            OutlinedButton.icon(
+              onPressed: _resetCounter,
+              style: OutlinedButton.styleFrom(
+                backgroundColor: Colors.grey.withOpacity(0.8),
+                foregroundColor: Colors.white,
+                side: BorderSide(color: Colors.grey, width: 2),
+                padding: EdgeInsets.symmetric(horizontal: 30),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              icon: Icon(Icons.restore),
+              label: const Text(
+                'Reset',
+                style: TextStyle(fontWeight: FontWeight.w900),
+              ),
             ),
           ],
         ),
